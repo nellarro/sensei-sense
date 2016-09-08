14629e9290ad6bbb375a0daf2ddc2f902d7cc177
Jenell Pizarro <nellarro@gmail.com>
2016-09-08 17:48:42 -0400

    Giving my tush a break from this lawn chair

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default () => {
-  return <div>
-    <h2>Title</h2>
-
-    <p>TODO: Describe how to play the game and make a play button</p>
-
+  return <div className='TitDirContainer'>
+    <h2>It must learn how to be one with the spirits.</h2>
+    <div className='TitDir'>
+      <p className='read'>To align the spirits one must read:</p>
+      <p className='directions'>The Sensei will choose four of six possible spirits. The Neophyte will use his or her <i>ki</i> to guess what four spirits the Sensei has chosen.</p>
+      <p> The Neophyte must learn that the Sensei will respond cryptically with markers.</p> <a className='white'>WHITE</a>: Correct spirit deciphered. <a className='red'>RED</a>: Incorrect spirit deciphered.
+    </div>
     <button><Link to='/game'>PLAY!</Link></button>
   </div>
 }
